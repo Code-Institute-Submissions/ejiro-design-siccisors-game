3710fc930921a7f35b4c4690f034817f4487d2f5
ejiro-design <ajirogene55@gmail.com>
2022-01-11 17:45:10 +0000

    setup function structure in place

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,19 @@ document.addEventListener(DOMException)
 function playGame(){
 
 }
+
+function checkScore(){
+
+}
+
+function incrementScore(){
+
+}
+
+function decrementScore(){
+
+}
+
+function drawScore(){
+    
+}
