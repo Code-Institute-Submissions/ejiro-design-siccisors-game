29d6aac3a0b1d41abd8537543c6263dac45a36ed
ejiro-design <ajirogene55@gmail.com>
2022-01-14 02:28:10 +0000

    adjust html script for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
-let score = document.getElementById("score-area")
-let draw = document.getElementsByClassId("draw")
-let lost = documen.getElementsByClassId("lost")
-let won = document.getElementById("won")
+document.addEventListener("DOMcontentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+      buttons.addEventListener("click", playGame)
+    }
+    let score = document.getElementById("score-area")
+    let draw = document.getElementsByClassId("draw")
+    let lost = documen.getElementsByClassId("lost")
+    let won = document.getElementById("won")
+})
+
 let result = {
     player: 0,
     computer:0,
 }
 // play game
-function playGAme(event){
+function playGame(event){
     console.log(event.target.id)
-
 }
 // event listener
-score-area.Foreach(score-area => score-area.addEventListener)
 
 function checkScore() {
 
