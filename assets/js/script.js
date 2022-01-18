4bbb5a4daf43d0bd97c86e7340e79c9647a27d39
ejiro-design <ajirogene55@gmail.com>
2022-01-18 02:47:25 +0000

    add script to get all the element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,3 @@
-document.addEventListener("DOMcontentLoaded", function(){
-    let buttons = document.getElementsByTagName("button");
-    for (let button of buttons){
-      buttons.addEventListener("click", playGame)
-    }
-    let score = document.getElementById("score-area")
-    let draw = document.getElementsByClassId("draw")
-    let lost = documen.getElementsByClassId("lost")
-    let won = document.getElementById("won")
-})
-
-let result = {
-    player: 0,
-    computer:0,
-}
-// play game
-function playGame(event){
-    console.log(event.target.id)
-}
-// event listener
-
-function checkScore() {
-
-}
-
-function incrementScore() {
-
-}
-
-function decrementScore() {
-
-}
-
-function drawScore() {
-
-}
+const choices = document.GetElementByClassName("choice")
+const score = document.getElementById("score-area")
+const replay = document.getElementById("replay")
