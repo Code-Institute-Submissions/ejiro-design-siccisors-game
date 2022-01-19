ef040bc997c10854e37c0e53c0f2ad54fd7438c2
ejiro-design <ajirogene55@gmail.com>
2022-01-19 04:05:56 +0000

    add start game function and event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 const choices = document.GetElementByClassName("choice")
 const score = document.getElementById("score-area")
 const replay = document.getElementById("replay")
+const scoreboard ={
+    draw = 0,
+    win = 0,
+    lost = 0
+}
+
+// start game
+function startGame(event){
+console.log(event.target.id)
+}
+// event listeners
+for(let choice of choices)
+    addEventListener("click", play)
