4a22ffd7841385b46a1adc537906b38501a1a925
ejiro-design <ajirogene55@gmail.com>
2022-01-19 05:46:59 +0000

    correct some mistakes on the choices variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
-const choices = document.GetElementByClassName("choice")
+const choices = document.getElementsByClassName("choice")
 const score = document.getElementById("score-area")
 const replay = document.getElementById("replay")
-const scoreboard ={
-    draw = 0,
-    win = 0,
-    lost = 0
-}
+const scoreboard = {
+    draw: 0,
+    win: 0,
+    lost: 0
+};
 
 // start game
 function startGame(event){
 console.log(event.target.id)
 }
 // event listeners
-for(let choice of choices)
-    addEventListener("click", startGame)
+for(let choice of choices){
+    choice.addEventListener("click", startGame)
+}
